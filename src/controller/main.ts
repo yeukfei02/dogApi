23b35984bcc6c1ddc94e5b989bedaa1e8caa4792
Donald Wu <yeukfei02@gmail.com>
2020-06-20 13:21:42 +0800

    feature/create-project-structure-#1: create project structure

---
+++ src/controller/main.ts
@@ -0,0 +1,9 @@
+import * as Koa from 'koa';
+
+export const getMain = async (ctx: Koa.Context, next: () => Promise<any>) => {
+    ctx.response.status = 200;
+    ctx.body = {
+        message: 'dogApi',
+    };
+    await next();
+};
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import * as Koa from 'koa';
+
+import * as cors from '@koa/cors';
+import * as helmet from 'koa-helmet';
+import * as logger from 'koa-logger';
+import * as compress from 'koa-compress';
+import * as bodyParser from 'koa-bodyparser';
+import * as json from 'koa-json';
+
+import mainRoutes from './routes/main';
+
+const app = new Koa();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(logger());
+app.use(compress({
+    filter (content_type) {
+        return /text/i.test(content_type)
+    },
+    threshold: 2048,
+    gzip: {
+        flush: require('zlib').Z_SYNC_FLUSH
+    },
+    deflate: {
+        flush: require('zlib').Z_SYNC_FLUSH,
+    },
+    br: false // disable brotli
+}));
+app.use(bodyParser());
+app.use(json());
+
+app.use(mainRoutes.routes());
+app.use(mainRoutes.routes()).use(mainRoutes.allowedMethods());
+
+app.listen(3000, () => {
+  console.log(`server is listening on port: ${port}`);
+});
