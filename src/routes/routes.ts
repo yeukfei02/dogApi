e506b171aee17388e2ed0d9c8fe3f17db82b725d
Donald Wu <yeukfei02@gmail.com>
2020-06-21 17:37:30 +0800

    fix-code: add koa-combine-router lib

---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+import * as combineRouters from 'koa-combine-routers';
+
+import mainRoutes from '../routes/main';
+import userRoutes from '../routes/user';
+import dogRoutes from '../routes/dog';
+
+const router = combineRouters(mainRoutes, userRoutes, dogRoutes);
+
+export default router;
