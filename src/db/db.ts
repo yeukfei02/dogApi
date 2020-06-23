2b13ae6f1cd1d9b3b7eeada8f421e9583cd935e5
Donald Wu <yeukfei02@gmail.com>
2020-06-23 15:39:45 +0800

    fix-code: update connectDB func

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,14 +4,28 @@ import { Dog } from '../entity/dog';
 import { DogImages } from '../entity/dogImages';
 
 export const connectDB = async () => {
-  await createConnection({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'donaldwu',
-    password: '',
-    database: 'donaldwu',
-    entities: [DogUser, Dog, DogImages],
-    synchronize: true,
-  });
+  const environment = process.env.NODE_ENV;
+  if (environment === 'development') {
+    await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'donaldwu',
+      password: '',
+      database: 'donaldwu',
+      entities: [DogUser, Dog, DogImages],
+      synchronize: true,
+    });
+  } else {
+    await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'donaldwu',
+      password: '',
+      database: 'donaldwu',
+      entities: [DogUser, Dog, DogImages],
+      synchronize: true,
+    });
+  }
 };
